c4f714fa88baefa75f17ff10422dc13e1f308698
JoHoYoung <whghdud17@naver.com>
2018-11-26 23:59:11 +0900

    no message

---
+++ batches/crawlingArticle.js
@@ -0,0 +1,1 @@
+const fetcher = require('../helper/fetcher')
---
+++ batches/distributeArticle.js
@@ -0,0 +1,11 @@
+const db = require('../helper/mysql')
+const helper = require('../helper/helper')
+
+const pool = db.pool
+
+pool.getConnection().then(async (conn) => {
+
+    let channels = 
+
+
+})
